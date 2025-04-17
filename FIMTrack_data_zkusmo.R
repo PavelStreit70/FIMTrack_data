c24b0242fa26faead4fe7f6f25e3607f4442e0da
PavelStreit70 <streit.pavel@email.cz>
2025-04-17 12:36:38 +0200

    dalsi_cast

--- FIMTrack_data_zkusmo.R
+++ FIMTrack_data_zkusmo.R
@@ -4,7 +4,7 @@
 #do GraphPadu
 
 library(readr)
-kontrola_1_varka <- read_csv("Rko/outputs/Prvni varka/output_2025-03-27_17-11-54/250324_1.csv")
+kontrola_1_varka <- read_csv("Rko/250324_1.csv")
 View(kontrola_1_varka)
 
 str(kontrola_1_varka)
@@ -57,4 +57,33 @@ kontrola_1_varka %>%
   summarise(across(starts_with("larva"), ~mean(.x, na.rm = TRUE)))
 
 
+#Vypsat celkovou uraženou vzdálenost acc_dist
+
+kontrola_1_varka %>%
+  filter(grepl("acc_dst", `...1`)) %>%  # Filtrování podle parametrů
+  slice_tail(n = 1)  # Získání posledního řádku
+
+kontrola_1_varka %>%
+  filter(grepl("acc_dst", `...1`)) %>%  # Filtrovat řádky, kde je 'acc_dst'
+  mutate(acc_dst_value = as.numeric(str_extract(`...1`, "(?<=acc_dst=)[0-9.]+"))) %>%  # Extrahujeme číslo za 'acc_dst='
+  filter(!is.na(acc_dst_value)) %>%  # Odstraníme řádky s NA
+  summarise(last_acc_dst = tail(acc_dst_value, 1))  # Získáme poslední hodnotu
+
+#Vypsat coiling a go phase - součty hodnot - 0 a 1
+
+library(dplyr)
+
+keywords <- c("coiling", "go_phase")  # Klíčová slova, která hledáme v textu
+
+kontrola_1_varka %>%
+  filter(Reduce(`|`, lapply(keywords, function(k) grepl(k, ...1)))) %>%  # Filtrovat podle klíčových slov
+  mutate(parametr = case_when(
+    grepl("coiling", ...1) ~ "coiling",  # Pokud obsahuje "coiling", označíme jako "coiling"
+    grepl("go_phase", ...1) ~ "go_phase"  # Pokud obsahuje "go_phase", označíme jako "go_phase"
+  )) %>%
+  group_by(parametr) %>%  # Skupina podle "parametr"
+  summarise(across(starts_with("larva"), ~mean(.x, na.rm = TRUE)))  # Průměr pro každý "larva" sloupec
+
+
 #Přidat podmínku s gophase - hýbaly se aspoň 50% času - půjde to/dáva to smysl?
+#Tuhle podmínku nechat na konec
